b38085b5953ac2a1948ee799177add8501ece585
Jacob Silver <jacobsilver2@mac.com>
2017-12-16 20:50:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,12 +60,12 @@ class TicTacToe
   end
 
   def won?
-  WIN_COMBINATIONS.detect do |combo|
-    @board[combo[0]] == @board[combo[1]] &&
-    @board[combo[1]] == @board[combo[2]] &&
-    position_taken?(combo[0])
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
+    end
   end
-end
 
 
 
